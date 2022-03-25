0eaa50bf4e91458c3fa8bd809ad7e5fdd342cc6b
Lincoln Anderson <landerson@threefivetwo.com>
2022-03-25 11:09:48 -0400

    add test-footer page

---
+++ pages/test-footer.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+import Footer from "../components/Footer";
+
+function TestFooter() {
+  return <React.Fragment>
+    <h1>Test Footer</h1>
+    <Footer />
+  </React.Fragment>;
+}
+
+export default TestFooter;
